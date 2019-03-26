49846c2cf288297032b50c1dc8bfe080db71cf07
Andrea <amerik22@yahoo.com>
2019-03-25 20:20:53 -0600

    initial commit

---
+++ clickygame/build/precache-manifest.fec4bc3d545cc891c125dbb376bdd88a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.038e769c.js"
+  },
+  {
+    "revision": "8a207409c962e3114f8b",
+    "url": "/ClickyGame/static/js/main.f92e5714.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/ClickyGame/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "8a207409c962e3114f8b",
+    "url": "/ClickyGame/static/css/main.13d8d3c5.chunk.css"
+  },
+  {
+    "revision": "160142ae9a5181f6baac0d15a091d8b3",
+    "url": "/ClickyGame/index.html"
+  }
+];
